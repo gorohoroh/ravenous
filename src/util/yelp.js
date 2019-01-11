0af6400a6d44d966686a1050adfa4c87de66250d
gorohoroh <gorohoroh@gmail.com>
2019-01-11 17:52:50 +0300

    Search that actually fetches data from Yelp API

---
+++ src/util/yelp.js
@@ -0,0 +1,30 @@
+import {YelpSecrets} from "../secrets";
+
+export const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+            { headers: {
+                Authorization: `Bearer ${YelpSecrets.apiKey}`
+            }
+        })
+        .then(response => response.json())
+        .then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.display_address,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                })
+            }
+        })
+    }
+};
